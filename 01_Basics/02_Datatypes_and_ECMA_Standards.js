2ea2a4aa50b651b8062a74d2f61505d0c5ee2101
pranavyamjal <yamjal0188@gmail.com>
2024-06-01 16:52:35 +0530

    Files

---
+++ 01_Basics/02_Datatypes_and_ECMA_Standards.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3
+    +
+    3               // code readability is important
+)
+
+console.log("Pranav")
+
+let name = "Pranav"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => object(typeof) | standalone value
+// undefined => undefined(typeof) | no value defined
+// symbol => unique
+// object
+
+console.log(typeof name);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
