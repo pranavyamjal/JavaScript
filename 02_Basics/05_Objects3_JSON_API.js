d9085ec0b04b1e4a953f81cb9bfaa255bcfb8b21
pranavyamjal <yamjal0188@gmail.com>
2024-06-02 19:01:11 +0530

    Objects3

---
+++ 02_Basics/05_Objects3_JSON_API.js
@@ -0,0 +1,32 @@
+// Object Destructuring
+
+const course = 
+{
+    coursename: "JavaScript",
+    price: "Free",
+    courseInstructor: "Pranav",
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course
+
+
+// console.log(courseInstructor)
+console.log(courseInstructor);
+
+
+// {
+//     "name": "Java"
+//     "coursename": "JavaScript"
+//     "price": "Free"
+// }
+
+//  RandomUser Generator API for visualing complex json structure
+// Json Formatter
+
+[
+    {},
+    {},
+    {}
+]
