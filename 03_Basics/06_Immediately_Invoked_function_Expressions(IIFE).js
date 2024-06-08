40969072592fc6b85f2c72371046b3eaea9f7311
pranavyamjal <yamjal0188@gmail.com>
2024-06-08 14:34:17 +0530

    IIFE

---
+++ 03_Basics/06_Immediately_Invoked_function_Expressions(IIFE).js
@@ -0,0 +1,14 @@
+(function chai()                    // named function
+{
+    console.log('DB CONNECTED')
+})
+chai();                             // calling function
+
+
+
+( (name) =>                         // anonymous function
+{
+    console.log(`Hello ${name}`)
+})
+("Pranav")                         // passing argument
+
