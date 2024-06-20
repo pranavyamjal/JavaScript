55f53cb3233594a9108caf9cbe110e2e720e75d3
pranavyamjal <yamjal0188@gmail.com>
2024-06-20 18:39:37 +0530

    Updated_Project1

--- 07_Projects/1_ColorChanger.js
+++ 07_Projects/1_ColorChanger.js
@@ -1,3 +1,5 @@
+// My code
+
 let color1 = document.getElementById('grey')
 let color2 = document.getElementById('white')
 let color3 = document.getElementById('blue')
@@ -17,4 +19,36 @@ color3.addEventListener('click', () => {
 
 color4.addEventListener('click', () => {
     document.body.style.backgroundColor = 'yellow'
+})
+
+
+// Below is the more efficent code
+
+const buttons = document.querySelectorAll('button')
+const body = document.querySelector('body')
+
+buttons.forEach( function (button)
+{
+    console.log(button)
+    button.addEventListener('click', function (e){
+        console.log(e)
+        console.log(e.target)
+
+        if(e.targer.id === 'grey'){
+            body.style.backgroundColor = 'grey'
+        }
+
+        if(e.targer.id === 'white'){
+            body.style.backgroundColor = 'white'
+        }
+
+        if(e.targer.id === 'blue'){
+            body.style.backgroundColor = 'blue'
+        }
+
+        if(e.targer.id === 'yellow'){
+            body.style.backgroundColor = 'yellow'
+        }
+
+    })
 })
